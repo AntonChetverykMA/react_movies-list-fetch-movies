b598185af8e8be31ce4bbb37a2bd5be9efe07812
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-13 20:59:54 +0200

    Updates

---
+++ precache-manifest.b5b9cfbf13aa4b6e28c6921d18fc3622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee576b513265b2eea52d9e62cfb654c7",
+    "url": "/react_movies-list-fetch-movies/index.html"
+  },
+  {
+    "revision": "4a53041e206e8f7eb2ad",
+    "url": "/react_movies-list-fetch-movies/static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "dbc941d7f88e6a71d516",
+    "url": "/react_movies-list-fetch-movies/static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "4a53041e206e8f7eb2ad",
+    "url": "/react_movies-list-fetch-movies/static/js/2.20a4f65f.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/react_movies-list-fetch-movies/static/js/2.20a4f65f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc941d7f88e6a71d516",
+    "url": "/react_movies-list-fetch-movies/static/js/main.4368002e.chunk.js"
+  },
+  {
+    "revision": "6449e3c728d279592b03",
+    "url": "/react_movies-list-fetch-movies/static/js/runtime-main.2ac049b9.js"
+  }
+]);
